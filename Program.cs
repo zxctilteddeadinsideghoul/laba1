0d775248fe85ab126a708020ce9ce9b75a016b0e
sadtowa <toni.afanasev.0505@mail.ru>
2023-10-26 14:05:00 +0500

    add Calculate func and move brackets

--- Program.cs
+++ Program.cs
@@ -4,13 +4,15 @@ using System.Collections.Generic;
 
 class Program{
 
-    public static readonly string[] numberSeparators = new string[] {" ", "+", "-", "/", "*"};
+    public static readonly string[] numberSeparators = new string[] {" ", "+", "-", "/", "*", "(", ")"};
     public static readonly string[] operationSeparators = new string[] {" ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
     
-    static List<string> Separate(string expression, string[] Separator){
+    static List<string> Separate(string expression, string[] Separator)
+    {
         List<string> result = new List<string>();
         
-        foreach(string elem in expression.Split(Separator, StringSplitOptions.RemoveEmptyEntries)){
+        foreach(string elem in expression.Split(Separator, StringSplitOptions.RemoveEmptyEntries))
+        {
             result.Add(elem);
         }
         
@@ -18,29 +20,34 @@ class Program{
     }
 
 
-    static double LegacyCalculate(List<string> numbers, List<string> operations){
-            List<double> rNumbers = numbers.ConvertAll(x => double.Parse(x));
-            int counter = operations.Count;
-            for (int i = 0; i < counter; i++){
-                switch(operations[i]){
-                    case ("*"):
-                        rNumbers[i] = rNumbers[i] * rNumbers[i+1];
-                        rNumbers.RemoveAt(i+1);
-                        operations.Remove("*");
-                        i--; counter--;
-                        break;
-                    case ("/"):
-                        rNumbers[i] = rNumbers[i] / rNumbers[i+1];
-                        rNumbers.RemoveAt(i+1);
-                        operations.Remove("/");//detete completed operation
-                        i--; counter--;
-                        break;
-                }
+    static double LegacyCalculate(List<string> numbers, List<string> operations)
+    {
+        List<double> rNumbers = numbers.ConvertAll(x => double.Parse(x));
+        int counter = operations.Count;
+        for (int i = 0; i < counter; i++)
+        {
+            switch(operations[i])
+            {
+                case ("*"):
+                    rNumbers[i] = rNumbers[i] * rNumbers[i+1];
+                    rNumbers.RemoveAt(i+1);
+                    operations.Remove("*");
+                    i--; counter--;
+                    break;
+                case ("/"):
+                    rNumbers[i] = rNumbers[i] / rNumbers[i+1];
+                    rNumbers.RemoveAt(i+1);
+                    operations.Remove("/");//detete completed operation
+                    i--; counter--;
+                    break;
+            }
+        }
 
-            }    
             counter = operations.Count;
-            for (int i = 0; i < counter; i++){
-                switch(operations[i]){
+            for (int i = 0; i < counter; i++)
+            {
+                switch(operations[i])
+                {
                     case ("+"):
                         rNumbers[i] = rNumbers[i] + rNumbers[i+1];
                         rNumbers.RemoveAt(i+1);
@@ -60,23 +67,37 @@ class Program{
         }
 
 
-        static double Calculate(List<string> numbers, List<string> operations)
+        static double ExpressionCalculate(List<string> numbers, List<string> operations)
         {
             List<double> rNumbers = numbers.ConvertAll(x => double.Parse(x));
-            return 1,2;
+            while(operations.Count > 0)
+            {
+                 
+            }
+
+            return 1.2;
         }
 
-
+    static float Calculate(string operation, float num1, float num2)
+    {
+        switch(operation)
+        {
+            case "+": return num1 + num2;
+            case "-": return num1 - num2;
+            case "*": return num1 * num2;
+            case "/": return num1 / num2;
+        }
+    }
     static void Main()
     {
         string exp = Console.ReadLine();
         List<string> numbers = Separate(exp, numberSeparators);
         List<string> operations = Separate(exp, operationSeparators);
         
-        Console.WriteLine(Calculate(numbers, operations));
+        //Console.WriteLine(Calculate(numbers, operations));
 
 
-        //Console.WriteLine(string.Join(" ", numbers));
-        //Console.WriteLine(string.Join(" ", operations));
+        Console.WriteLine(string.Join(" ", numbers));
+        Console.WriteLine(string.Join(" ", operations));
     }
 }
